2517ea08d64d3b0507ffeaf74701c10292c50da3
datadiego <juandiegomariscal@gmail.com>
2023-12-05 20:49:14 +0100

    NEW: Clase de partículas básicas

---
+++ 00_basic.js
@@ -0,0 +1,36 @@
+class Particle {
+    constructor(){
+        this.x = width/2;
+        this.y = height/2;
+        this.r = random(20);
+        this.velX = random(-1, 1);
+        this.velY = random(-1, 1);
+        this.gray = random(255);
+    }
+    show(){
+        noStroke();
+        fill(this.gray);
+        ellipse(this.x, this.y, this.r);
+    }
+    update(){
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+}
+
+let particles = [];
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    for(let i = 0; i < 100; i++){
+        particles.push(new Particle());
+    }
+}
+
+function draw(){
+    background(100);
+    for(let i = 0; i < particles.length; i++){
+        particles[i].show();
+        particles[i].update();
+    }
+}
